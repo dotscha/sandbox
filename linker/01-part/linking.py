a6564df7accd1ceb72f0a11fe4a2fa532fc89c53
dotscha <dotsch@gmail.com>
2017-04-24 15:20:39 +0200

    Update linking.py

--- linker/01-part/linking.py
+++ linker/01-part/linking.py
@@ -1,8 +1,8 @@
 meta = {
 	'name'       : "part1",
-	'init'       : 0x1000,
-	'start'      : 0x1003,
-	'finish'     : 0x1006,
+	'init'       : 0x2000,
+	'start'      : 0x2003,
+	'finish'     : 0x2006,
 	'requires'   : ["prg1.prg","prg2.prg","data.bin@2000"],
 	'reused'     : "music.sid[$7e:]@$1000",
 	'uses'       : [(0x02,0xef),(0x0800,0x7fff)],
